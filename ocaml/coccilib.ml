0b7bfb55f67d2559c986a00edff94ff78d7f1c8a
Gabriel Scherer <gabriel.scherer@gmail.com>
2016-03-15 12:04:19 +0100

    Remove the exposed_modules.ml indirection, expose modules in coccilib
    The fix of commit c7cf750d1c44fe28535306628f34d7133611a948 weakens the
    type signature of the Exposed_modules module, in a way that restores
    the .cmi behavior of older OCaml releases, but also does not combine
    well with the re-strengthening that happens when doing 'include
    Exposed_modules' from Coccilib.
    
    This patch inlines the content of Exposed_modules in Coccilib
    directly, which gets rid of the double-indirection, and results in
    stronger type equalities. The regression test file tests/mdeclp2.cocci
    passes after this change.
    
    Long-term I think that it would be nice to have an explicit
    coccilib.mli interface with documentation comments, and coccilib.ml
    with implementation only. Then you could do either
      coccilib.ml:
        module Foo = Foo
      coccilib.mli:
        module Foo = module type of Foo
    or
      coccilib.ml:
        module Foo = struct include Foo end
      coccilib.mli:
        module Foo = module type of Foo
    both should work (the first being simpler).

--- ocaml/coccilib.ml
+++ ocaml/coccilib.ml
@@ -7,7 +7,36 @@ open Common
 (**/**)
 (** Coccinelle modules accessible from an ocaml script.
 *)
-include Exposed_modules
+(*
+ We use
+ module Foo = struct include Bar end
+ rather than
+ module Foo = Bar
+ Because of an incompatible change in the interpretation of
+ module aliases introduced in OCaml 4.02.1.
+ See http://caml.inria.fr/mantis/view.php?id=6661
+*)
+
+module Ast_c    = struct include Ast_c end
+module Parse_c  = struct include Parse_c end
+module Parser_c = struct include Parser_c end
+module Lexer_c  = struct include Lexer_c end
+module Pretty_print_c = struct include Pretty_print_c end
+module Lib_parsing_c  = struct include Lib_parsing_c end
+module Visitor_c      = struct include Visitor_c end
+
+module Regexp     = struct include Regexp end
+module Config     = struct include Config end
+module Flag       = struct include Flag end
+module Iteration  = struct include Iteration end
+module Common     = struct include Common end
+
+module Ast_cocci  = struct include Ast_cocci end
+module Ast0_cocci = struct include Ast0_cocci end
+module Type_cocci = struct include Type_cocci end
+
+module Dumper = struct include Dumper end
+
 (**/**)
 
 (** A value of type {b pos} describes a position in a source file.
